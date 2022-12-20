605656757926083fed6a997a671d893aacff2e11
DH <DH@github.com>
2022-12-20 18:59:00 +0530

    first release

---
+++ test1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+const char* name1;
+const char* name2;
+printf("Get Journal-------                  ----------");
+printf("(Dr)___(Cr)___\n");
+scanf("%s",&name1,&name2);
+
+printf("%s",name1);
+}
